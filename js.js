c7d87c2172b8421a2adfcd83ab10629b354a312d
Bun-the-dog <118442912+Bun-the-dog@users.noreply.github.com>
2023-07-31 00:28:26 +0100

    Update js.js

--- js.js
+++ js.js
@@ -3,5 +3,5 @@ const button = document.querySelector('button');
 
 /* Тут на кнопку навешиваем обрабочик, который ждёт клика и тогда запустит логику */
 button.addEventListener('click', function() {
-	alert("If it's later than 10 p.m. and you think it's still a great idea, don't do it 😉")
+	alert("🗳️ Takk for din stemme og støtte til kandidat Nr. 23 Nathalie Jarmstad! Avstemningslenken vil være aktiv fra 11. august 2023. Avgi stemmen din via lenken eller besøk www.test.no. Din stemme betyr noe, så la oss gjøre en forskjell sammen! Din aktive deltakelse kan forme fremtiden til vår by.")
 })
