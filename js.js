3fa08c23fc84f75faa10258cc1f4a7abb362ec6d
Bun-the-dog <118442912+Bun-the-dog@users.noreply.github.com>
2023-07-31 21:30:27 +0100

    Delete js.js

--- js.js
+++
@@ -1,7 +0,0 @@
-/* Это объявление переменной, мы наши кнопку по тегу */
-const button = document.querySelector('button');
-
-/* Тут на кнопку навешиваем обрабочик, который ждёт клика и тогда запустит логику */
-button.addEventListener('click', function() {
-	alert("🗳️ Takk for din stemme og støtte til kandidat Nr. 23 Nathalie Jarmstad! Avstemningslenken vil være aktiv fra 11. august 2023. Avgi stemmen din via lenken eller besøk www.test.no. Din stemme betyr noe, så la oss gjøre en forskjell sammen! Din aktive deltakelse kan forme fremtiden til vår by.")
-})
